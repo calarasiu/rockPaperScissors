c0d5e0bb08ae6adb5aaf0eb7617958fbb5624319
cornelia chirila <cornelia.calarasiu@yahoo.it>
2021-04-17 00:01:18 +0300

    add style

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
 let userChosen;
 let computerChosen;
-const randomNumber = Math.round(Math.random()*(3));
+let userScore = 0;
+let computerScore=0;
 const displayResult = document.getElementById("result");
 const computerChoice = document.getElementById("computer-choice");
 const userChoice = document.getElementById("user-choice");
 const possibleChoices = document.querySelectorAll(".choices");
+const userScoreElement = document.getElementById("userScore");
+const computerScoreElement = document.getElementById("computerScore");
 var result = results();
+
+function refresh(){
+  window.location.reload("Refresh")
+}
 
 possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener('click', (e)=> {
   userChosen = e.target.id
   generatedComputerChoice()
+  console.log(computerChosen);
   results()
   userChoice.innerHTML = userChosen;
   computerChoice.innerHTML = computerChosen;
   displayResult.innerHTML = result;
-
+  userScoreElement.innerHTML = userScore;
+  computerScoreElement.innerHTML = computerScore;
+  
 }))
 
 function generatedComputerChoice(){
-  if(randomNumber === 1){
+  const randomNumber = Math.round(Math.random()*(3));
+  if(randomNumber <= 1){
     console.log(randomNumber);
-    return computerChosen = 'rock';
-  }else if(randomNumber === 2){
+    return computerChosen = '🌑rock';
+  }else if(randomNumber <= 2){
     console.log(randomNumber);
-    return computerChosen = 'paper';
+    return computerChosen = '🧾paper';
   }
-  else if (randomNumber === 3){
+  else if (randomNumber <= 3){
     console.log(randomNumber);
-    return computerChosen = 'scissors'
+    return computerChosen = '✂scissors'
   }
 }
 
 //get results
 function results(){
   if(computerChosen === userChosen){
-    return result = 'It is a Tie';
-  }else if(computerChosen === 'rock'&& userChosen === 'scissors'){
-    return result = 'You lost';
-  }else if(computerChosen === 'rock'&& userChosen === 'paper'){
-    return result = 'You win';
-  }else if(computerChosen === 'paper'&& userChosen === 'rock'){
-    return result = 'You lost';
-  }else if(computerChosen === 'paper'&& userChosen === 'scissors'){
-    console.log('You win');
-    return result = 'You win';
-  }else if(computerChosen === 'scissors'&& userChosen === 'paper'){
-    return result = 'You lost';
-  }else if(computerChosen === 'scissors'&& userChosen === 'rock'){
-    return result = 'You win';  
+    return result = '🏳 It is a Tie ';
+  }else if(computerChosen === '🌑rock'&& userChosen === '✂scissors'){
+    computerScore+=1;
+    return result = '🙈 You lost';
+  }else if(computerChosen === '🌑rock'&& userChosen === '🧾paper'){
+    userScore+=1;
+    return result = '😎 You win';
+  }else if(computerChosen === '🧾paper'&& userChosen === '🌑rock'){
+    computerScore+=1;
+    return result = '🙈 You lost';
+  }else if(computerChosen === '🧾paper'&& userChosen === '✂scissors'){
+    userScore+=1;
+    return result = '😎 You win';
+  }else if(computerChosen === '✂scissors'&& userChosen === '🧾paper'){
+    computerScore+=1;
+    return result = '🙈 You lost';
+  }else if(computerChosen === '✂scissors'&& userChosen === '🌑rock'){
+    userScore+=1;
+    return result = '😎 You win';  
   }
 }
